29bd3e9549548423c01d744774e384e372864d24
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2023-02-17 20:43:59 -0500

    java-10: Arrays.CopyOf

--- src/test/java/copy/of/CopyOfTest.java
+++ src/test/java/copy/of/CopyOfTest.java
@@ -7,9 +7,17 @@ import java.util.Arrays;
 public class CopyOfTest {
 
     @Test
-    public void methodTest() {
+    public void arrayscopyOf() {
         int[] numeros = {1, 2, 3, 4, 5};
         int[] copiaNumeros = Arrays.copyOf(numeros, 3);
     }
 
+    @Test
+    public void arrayscopyOfModify() {
+        int[] numeros = {1, 2, 3, 4, 5, 0};
+        int[] copiaNumeros = Arrays.copyOf(numeros, numeros.length);
+        numeros[5]=6;
+    }
+
 }
+
